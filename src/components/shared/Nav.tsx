9ffa739bc4d9c5103ea70fef71c6019f1c0513ba
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 00:04:31 +0600

    Updated

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -1,9 +1,19 @@
-import { Link } from "react-scroll";
-import { BiHome, BiUser } from "react-icons/bi";
+import { Link } from 'react-scroll';
+import { BiHomeAlt, BiUser } from "react-icons/bi";
 import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
 
 const Nav = () => {
-	return <div>Nav</div>;
+	return (
+		<nav className='fixed bottom-2'>
+			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
+				<div>
+					<Link to=''>
+						<BiHomeAlt />
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
