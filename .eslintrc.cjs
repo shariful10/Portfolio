8b9d0425813bc25c1b57eca30454c60cabe1b80d
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-10 20:24:07 +0600

    Updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,22 @@
 module.exports = {
 	root: true,
-	env: { browser: true, es2023: true, node: true },
+	env: { browser: true, es2023: true, Node: true },
 	extends: [
 		"eslint:recommended",
+		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:react-hooks/recommended",
 	],
 	ignorePatterns: ["dist", ".eslintrc.cjs"],
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
+	plugins: ["react-refresh"],
 	rules: {
 		"react-refresh/only-export-components": [
 			"warn",
 			{ allowConstantExport: true },
 		],
-		"no-unused-vars": ["error", { ignoreRestSiblings: true }],
+		"react/prop-types": "off",
+		"react/react-in-tsx-scope": "off",
+		"react/no-unescaped-entities": 0,
 	},
 };
