36fe4a335e1fab09330febfff201bf85e7419fc4
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 15:23:59 +0600

    Nav Icon Implemented

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 import { BiHomeAlt, BiUser } from "react-icons/bi";
 import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
 
 const Nav = () => {
 	return (
-		<nav className='fixed bottom-2'>
+		<nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
 			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
-				<div>
-					<Link to=''>
+				<div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+					<Link to="" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
 						<BiHomeAlt />
+					</Link>
+					<Link to="" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+						<BiUser />
+					</Link>
+					<Link to="" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+						<BsClipboardData />
+					</Link>
+					<Link to="" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+						<BsBriefcase />
 					</Link>
 				</div>
 			</div>
