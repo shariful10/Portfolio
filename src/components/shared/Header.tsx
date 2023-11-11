67637f37b25f1cfc72b5591916252ce3b43ce865
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 22:42:06 +0600

    Logo Implemented On Header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,5 +1,22 @@
+
 const Header = () => {
-	return <div className="bg-pink-300">Header</div>;
+	return (
+		<header className="bg-pink-300 py-8">
+			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
+				<div className="">
+					<a href="#">
+						<h1 className="text-xl xl:text-[36px] leading-[24px] xl:leading-[40px] font-black font-primary">
+							<span className="text-transparent bg-clip-text bg-gradient-to-r from-[#FF56F6] via-[#B936EE] to-[#406AFF]">
+								SHARIFUL
+							</span> 
+							<br />
+							ISLAM
+						</h1>
+					</a>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
