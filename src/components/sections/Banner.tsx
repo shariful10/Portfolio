505ffbb233ffd440c4219cd13cf153810511e655
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 23:32:41 +0600

    TypeAnimation Implemented On Banner

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -8,13 +8,40 @@ const Banner = () => {
 	return (
 		<div id="home" className="section">
 			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
-            <div className="">
-               <div className="">text</div>
-               <div className="">
-                  <img src={Image} alt="" />
-               </div>
-            </div>
-         </div>
+				<div className="">
+					<div className="">
+						<h1 className="text-xl md:text-2xl md:leading-[28px] xl:text-[32px] leading-[24px] xl:leading-[36px] font-black font-primary">
+							<span className="text">SHARIFUL</span>
+							<br />
+							ISLAM
+						</h1>
+						<div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold">
+							<span className="mr-4">I am a</span>
+							<TypeAnimation
+								sequence={[
+                           "Full Stack Developer",
+									2000,
+									"React Developer",
+									2000,
+									"Frontend Developer",
+									2000,
+									"MERN Stack Developer",
+									2000,
+									"Wordpress Developer",
+									2000,
+								]}
+                        speed={50}
+                        className="text-accent"
+                        wrapper="span"
+                        repeat={Infinity}
+							/>
+						</div>
+					</div>
+					<div className="">
+						<img src={Image} alt="" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
