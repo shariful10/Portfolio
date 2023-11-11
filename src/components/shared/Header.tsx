252d935743704b201089fcf2d05a422fc4da6a11
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 23:03:16 +0600

    Button Implemented On Header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,18 +1,19 @@
 
 const Header = () => {
 	return (
-		<header className="bg-pink-300 py-8">
+		<header className="py-8">
 			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
-				<div className="">
+				<div className="flex items-center justify-between">
 					<a href="#">
-						<h1 className="text-xl xl:text-[36px] leading-[24px] xl:leading-[40px] font-black font-primary">
-							<span className="text-transparent bg-clip-text bg-gradient-to-r from-[#FF56F6] via-[#B936EE] to-[#406AFF]">
+						<h1 className="text-xl md:text-2xl md:leading-[28px] xl:text-[36px] leading-[24px] xl:leading-[40px] font-black font-primary">
+							<span className="text">
 								SHARIFUL
 							</span> 
 							<br />
 							ISLAM
 						</h1>
 					</a>
+               <button className="btn btn-sm">Work with me</button>
 				</div>
 			</div>
 		</header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ export default {
 			colors: {
 				primary: "#0a0a0a",
 				accent: "#B809C3",
+            logo: "linear-gradient(92.23deg, #ff56f6 21.43%, #b936ee 50.63%, #3bace2 100%,  #406aff 117.04%)"
 			},
 			backgroundImage: {
 				site: "url('./assets/site-bg.jpg')",
