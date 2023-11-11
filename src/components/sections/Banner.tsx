f5c449909c6febc5510ee84fb03c7b5ed0f66370
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 23:20:28 +0600

    Image Implemented On Banner

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -1,5 +1,22 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import Image from "../../assets/avatar.svg";
+import { TypeAnimation } from "react-type-animation";
+import { FaGithub, FaLinkedin, FaDribbble } from "react-icons/fa";
+
 const Banner = () => {
-	return <div id="home" className="section">Banner</div>;
+	return (
+		<div id="home" className="section">
+			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
+            <div className="">
+               <div className="">text</div>
+               <div className="">
+                  <img src={Image} alt="" />
+               </div>
+            </div>
+         </div>
+		</div>
+	);
 };
 
 export default Banner;
