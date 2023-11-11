3d92c2c9f2d98403a22d823872b6d25a675723f9
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-11 16:41:06 +0600

    Link Customize

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -7,19 +7,49 @@ const Nav = () => {
 		<nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
 			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
 				<div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
-					<Link to="home" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+					<Link
+						to="home"
+						activeClass="active"
+						smooth={true}
+                  spy={true}
+						className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+					>
 						<BiHomeAlt />
 					</Link>
-					<Link to="about" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+					<Link
+						to="about"
+						activeClass="active"
+						smooth={true}
+                  spy={true}
+						className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+					>
 						<BiUser />
 					</Link>
-					<Link to="services" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+					<Link
+						to="services"
+						activeClass="active"
+						smooth={true}
+                  spy={true}
+						className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+					>
 						<BsClipboardData />
 					</Link>
-					<Link to="work" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+					<Link
+						to="work"
+						activeClass="active"
+						smooth={true}
+                  spy={true}
+						className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+					>
 						<BsBriefcase />
 					</Link>
-					<Link to="contact" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+					<Link
+						to="contact"
+						activeClass="active"
+						smooth={true}
+                  spy={true}
+						className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+					>
 						<BsChatSquareText />
 					</Link>
 				</div>
