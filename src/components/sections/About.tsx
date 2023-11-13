e79528690b750680d658b663116f0182f8449504
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-13 20:13:12 +0600

    Famer Motion Implemented On About Me Section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -12,11 +12,23 @@ const About = () => {
 		<section id="about" className="section" ref={ref}>
 			<div className="w-full md:w-5/6 md:mx-auto px-5 md:px-0">
 				<div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
-					<div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
-					<div className="flex-1">
+					<motion.div
+						variants={fadeIn({ direction: "right", delay: 0.3 })}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+					></motion.div>
+					<motion.div
+						variants={fadeIn({ direction: "left", delay: 0.5 })}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1"
+					>
 						<h2 className="h2 text-accent">About me.</h2>
 						<h3 className="h3 mb-4">
-							I'm a MERN Stack Developer with over 1 yaer of experience
+							I'm a MERN Stack Developer with over 2 yaers of experience
 						</h3>
 						<p className="mb-6">
 							This is Shariful Islam, a MERN Stack developer passionate
@@ -31,7 +43,8 @@ const About = () => {
 								<div className="text-[40px] font-tertiary text-gradient mb-2">
 									{inView && (
 										<CountUp start={0} end={2} duration={3} />
-									)}+
+									)}
+									+
 								</div>
 								<div className="font-primary text-sm tracking-[2px]">
 									Years of <br />
@@ -42,7 +55,8 @@ const About = () => {
 								<div className="text-[40px] font-tertiary text-gradient mb-2">
 									{inView && (
 										<CountUp start={0} end={100} duration={3} />
-									)}+
+									)}
+									+
 								</div>
 								<div className="font-primary text-sm tracking-[2px]">
 									Projects <br />
@@ -53,7 +67,8 @@ const About = () => {
 								<div className="text-[40px] font-tertiary text-gradient mb-2">
 									{inView && (
 										<CountUp start={0} end={35} duration={3} />
-									)}+
+									)}
+									+
 								</div>
 								<div className="font-primary text-sm tracking-[2px]">
 									Setisfied <br />
@@ -61,11 +76,13 @@ const About = () => {
 								</div>
 							</div>
 						</div>
-                  <div className="flex gap-x-8 items-center">
-                     <button className="btn btn-lg">Contact me</button>
-                     <a href="#" className="text-gradient btn-link">My Portfolio</a>
-                  </div>
-					</div>
+						<div className="flex gap-x-8 items-center">
+							<button className="btn btn-lg">Contact me</button>
+							<a href="#" className="text-gradient btn-link">
+								My Portfolio
+							</a>
+						</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
